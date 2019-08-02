916b9f8cdc91936f2509d56336bfd3e7d5ed4a12
andrefangeloni <andrefangeloni@gmail.com>
2019-08-02 09:10:21 -0300

    Dados consumidos da API

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -3,22 +3,22 @@ import { MdAdd } from "react-icons/md";
 import Card from "../Card";
 import { Container } from "./styles";
 
-export default function List() {
+export default function List({ data }) {
   return (
     <Container>
       <header>
-        <h2>Tarefas</h2>
-        <button type="button">
-          <MdAdd size={24} color="#fff" />
-        </button>
+        <h2>{data.title}</h2>
+        {data.creatable && (
+          <button type="button">
+            <MdAdd size={24} color="#fff" />
+          </button>
+        )}
       </header>
 
       <ul>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {data.cards.map(card => (
+          <Card key={card.id} data={card} />
+        ))}
       </ul>
     </Container>
   );
